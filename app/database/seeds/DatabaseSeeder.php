6420bc660873786e43bf015df75801518be78911
tumregels <levon.ghasabyan@gmail.com>
2015-05-27 11:03:17 +0400

    adds DatabaseSeeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -9,9 +9,66 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Eloquent::unguard();
+		//Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('DataTableSeeder');
+
+        $this->command->info('User table seeded!');
 	}
+
+}
+
+class DataTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('data')->delete();
+
+        $path = storage_path().DIRECTORY_SEPARATOR.'pdfs'.DIRECTORY_SEPARATOR;
+
+        $fullpath = $path.'resume_1.pdf';
+        $d = new Data;
+        $d-> firstname = 'Gago' ;
+        $d-> lastname = 'Arsenyan' ;
+        $d-> keywords = 'php, mysql, ruby' ;
+        $d->pdffile = File::name($fullpath) . '.pdf';
+        $d->type = File::extension($fullpath);
+        $d->file = base64_encode(File::get($fullpath));
+        $d->mime = 'application/pdf';
+        $d->save();
+
+        $fullpath = $path.'resume_2.pdf';
+        $d = new Data;
+        $d-> firstname = 'Nathan' ;
+        $d-> lastname = 'Serakuz' ;
+        $d-> keywords = 'php, laravel';
+        $d->pdffile = File::name($fullpath) . '.pdf';
+        $d->type = File::extension($fullpath);
+        $d->file = base64_encode(File::get($fullpath));
+        $d->mime = 'application/pdf';
+        $d->save();
+
+        $fullpath = $path.'resume_3.pdf';
+        $d = new Data;
+        $d-> firstname = 'Robin' ;
+        $d-> lastname = 'Erikson' ;
+        $d-> keywords = 'python, django' ;
+        $d->pdffile = File::name($fullpath) . '.pdf';
+        $d->type = File::extension($fullpath);
+        $d->file = base64_encode(File::get($fullpath));
+        $d->mime = 'application/pdf';
+        $d->save();
+
+        $fullpath = $path.'resume_4.pdf';
+        $d = new Data;
+        $d-> firstname = 'Angelina' ;
+        $d-> lastname = 'Gustafson' ;
+        $d-> keywords = 'ruby, RubyOnRails' ;
+        $d->pdffile = File::name($fullpath) . '.pdf';
+        $d->type = File::extension($fullpath);
+        $d->file = base64_encode(File::get($fullpath));
+        $d->mime = 'application/pdf';
+        $d->save();
+    }
 
 }
--- app/routes.php
+++ app/routes.php
@@ -16,11 +16,63 @@ Route::get('/', 'DataController@index');
 Route::get('/create', 'DataController@create');
 Route::post('/create', 'DataController@handleCreate');
 
-Route::get('search', 'DataController@search');
-Route::post('search', 'DataController@handleSearch');
+Route::get('/search', 'DataController@search');
+Route::post('/search', 'DataController@handleSearch');
 
-Route::get('detailedsearch', 'DataController@detailedSearch');
-Route::post('detailedsearch', 'DataController@handleDetailedSearch');
+Route::get('/detailedsearch', 'DataController@detailedSearch');
+Route::post('/detailedsearch', 'DataController@handleDetailedSearch');
 
 Route::get('/delete/{id}', 'DataController@delete');
 Route::post('/delete', 'DataController@handleDelete');
+
+Route::get('/seed', function()
+{
+    $path = storage_path().DIRECTORY_SEPARATOR.'pdfs'.DIRECTORY_SEPARATOR;
+
+    $fullpath = $path.'resume_1.pdf';
+    $d = new Data;
+    $d-> firstname = 'Gago' ;
+    $d-> lastname = 'Arsenyan' ;
+    $d-> keywords = 'php, mysql, ruby' ;
+    $d->pdffile = File::name($fullpath) . '.pdf';
+    $d->type = File::extension($fullpath);
+    $d->file = base64_encode(File::get($fullpath));
+    $d->mime = 'application/pdf';
+    $d->save();
+
+    $fullpath = $path.'resume_2.pdf';
+    $d = new Data;
+    $d-> firstname = 'Nathan' ;
+    $d-> lastname = 'Serakuz' ;
+    $d-> keywords = 'php, laravel';
+    $d->pdffile = File::name($fullpath) . '.pdf';
+    $d->type = File::extension($fullpath);
+    $d->file = base64_encode(File::get($fullpath));
+    $d->mime = 'application/pdf';
+    $d->save();
+
+    $fullpath = $path.'resume_3.pdf';
+    $d = new Data;
+    $d-> firstname = 'Robin' ;
+    $d-> lastname = 'Erikson' ;
+    $d-> keywords = 'python, django' ;
+    $d->pdffile = File::name($fullpath) . '.pdf';
+    $d->type = File::extension($fullpath);
+    $d->file = base64_encode(File::get($fullpath));
+    $d->mime = 'application/pdf';
+    $d->save();
+
+    $fullpath = $path.'resume_4.pdf';
+    $d = new Data;
+    $d-> firstname = 'Angelina' ;
+    $d-> lastname = 'Gustafson' ;
+    $d-> keywords = 'ruby, RubyOnRails' ;
+    $d->pdffile = File::name($fullpath) . '.pdf';
+    $d->type = File::extension($fullpath);
+    $d->file = base64_encode(File::get($fullpath));
+    $d->mime = 'application/pdf';
+    $d->save();
+
+
+    echo 'Database seeded';
+});
